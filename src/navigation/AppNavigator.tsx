396314036051eeb79bd19fb9596edc3ecb466a4c
Kyle Bateman <kyle@batemans.org>
2025-11-12 20:38:41 -0700

    Basic home screen working

--- src/navigation/AppNavigator.tsx
+++ src/navigation/AppNavigator.tsx
@@ -1,7 +1,7 @@
 import React from 'react';
 import { NavigationContainer } from '@react-navigation/native';
 import { createNativeStackNavigator } from '@react-navigation/native-stack';
-import Home from '../screens/Home';
+import ConnectionsList from '../screens/ConnectionsList';
 
 const Stack = createNativeStackNavigator();
 
@@ -9,7 +9,7 @@ export default function AppNavigator() {
   return (
     <NavigationContainer>
       <Stack.Navigator>
-        <Stack.Screen name="Home" component={Home} options={{ title: 'Sereus Chat' }} />
+        <Stack.Screen name="ConnectionsList" component={ConnectionsList} options={{ title: 'Sereus Chat' }} />
       </Stack.Navigator>
     </NavigationContainer>
   );
